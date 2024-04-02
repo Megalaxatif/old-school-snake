61a13e6cf55b5cdb644b3eb6e382b8237053da71
Megalaxatif <mr.megalaxatif@gmail.com>
2024-04-02 23:41:33 +0200

    commit 1

--- snake.cpp
+++ snake.cpp
@@ -1,28 +1,29 @@
 #include "snake.h"
 
+const char* ANSI_code[4] = {"\033[37m  \033[0m", "\033[37;47m@@\033[0m", "\033[31m[]\033[0m", "\033[32;1m[]\033[0m"}; // empty space, border, fruit, snake
 
-const char* ANSI_code[2] = {"\033[37m  \033[0m", "\033[37;47m@@\033[0m"};
-
-Snake::Snake() : board{ 0 }
-{
-    srand((unsigned)time(NULL));
-    board_length = 10;
-    board_heigth = 10;
+Snake::Snake() : board{ 0 } {
+    snake_length = 0;
     posx = board_length/2;
     posy = board_heigth/2;
     x_array[0] = posx;
     y_array[0] = posy;
+    apple_posx = 0;
+    apple_posy = 0;
+    board_length = 10;
+    board_heigth = 15;
+    srand((unsigned)time(NULL));
 }
 void Snake::init_board(){
     for (int y = 0; y < board_heigth; y++) {
         for (int x = 0; x < board_length; x++) {
-            board[0][y] = board[board_length - 1][y] = board[x][0] = board[x][board_heigth - 1] = 1; // we initialise all edges to 1 (integer for the edge of the board)
+            board[0][y] = board[board_length - 1][y] = board[x][0] = board[x][board_heigth - 1] = (int)INDEX::BORDER;
         }
     }
 }
 void Snake::draw_board(){
-    for (int i = 0; i < board_length; i++){
-        for (int j = 0; j < board_heigth; j++){
+    for (int i = 0; i < board_heigth; i++){
+        for (int j = 0; j < board_length; j++){
             std::cout << ANSI_code[board[j][i]];
             //std::cout << board[j][i];
         }
@@ -30,10 +31,29 @@ void Snake::draw_board(){
     }
 }
 void Snake::spawn_apple(){
-    while (board[apple_posx = rand() % board_length][apple_posy = rand() % board_heigth] != 0){}
-    board[apple_posx][apple_posy] = 1;
+    while (board[apple_posx = rand() % board_length][apple_posy = rand() % board_heigth] != (int)INDEX::EMPTY_SPACE){}
+    board[apple_posx][apple_posy] = (int)INDEX::FRUIT;
 }
-void Snake::move_snake(){
+void Snake::input_handler(){
+     if (GetAsyncKeyState(VK_RIGHT) & 0x8000) {
+     }
+     if (GetAsyncKeyState(VK_LEFT) & 0x8000) {
+     }
+     if (GetAsyncKeyState(VK_UP) & 0x8000) {
+     }
+     if (GetAsyncKeyState(VK_DOWN) & 0x8000) {
+     }
+}
+void Snake::move_snake(int dirx, int diry){
+    if (board[posx + dirx][posy + diry] == (int)INDEX::EMPTY_SPACE || board[posx + dirx][posy + diry] == (int)INDEX::FRUIT){ // the snake moves only if there is an empty space or a fruit right in front of it
+        // we set the new value of posx and posy
+        posx = posx + dirx;
+        posy = posy + diry;
+        // we set the new position of the head
+        x_array[snake_length] = posx;
+        y_array[snake_length] = posy;
+    }
+    // we move the array
     for(int i = 0; i < snake_length - 1; i++){
         x_array[i] = x_array[i + 1];
         y_array[i] = y_array[i + 1];
--- snake.h
+++ snake.h
@@ -2,9 +2,16 @@
 #define SNAKE_H
 
 #include <iostream>
-#include <conio.h>
+#include <windows.h>
 #include <string>
 #include <ctime>
+
+enum class INDEX{
+    EMPTY_SPACE = 0,
+    BORDER = 1,
+    FRUIT = 2,
+    SNAKE = 3
+};
 
 class Snake{
     public:
@@ -12,19 +19,19 @@ class Snake{
         void init_board();
         void draw_board();
         void spawn_apple();
-        void check_collision();
-        void move_snake();
+        void input_handler();
+        void move_snake(int dirx, int diry);
     private:
         int snake_length;
-        int apple_posx;
-        int apple_posy;
         int posx;
         int posy;
         int x_array[100];
         int y_array[100];
+        int apple_posx;
+        int apple_posy;
         int board_length;
         int board_heigth;
-        int board[10][10];
+        int board[10][15];
 
 };
 #endif
